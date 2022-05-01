dfb5b7461df974b81c28734d14b376bfff5bc4da
iktalk <iktalk.94@gmail.com>
2022-05-01 14:54:11 -0400

    Add search bar

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,34 @@
 import React from "react";
 
 export default function Weather() {
-  return <h1>Hello from Weather</h1>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autofocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Toronto</h1>
+      <h2>Sunday May 1</h2>
+      <ul>
+        <li>Cloudy</li>
+        <li>Humidity</li>
+        <li>Wind</li>
+      </ul>
+    </div>
+  );
 }
