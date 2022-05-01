691a401543041b8369c0bd95bb40c524aa2981d5
iktalk <iktalk.94@gmail.com>
2022-05-01 14:34:15 -0400

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <p>
-        This website is coded by Ishmeet Kaur and open source on{" "}
-        <a
-          href="https://github.com/iktalk/weather-app-react.git"
-          target="_blank"
-        >
-          Github
-        </a>
-      </p>
+    <div className="container">
+      <div className="App">
+        <h1>Hello</h1>
+        <p>
+          This website is coded by Ishmeet Kaur and open source on{" "}
+          <a
+            href="https://github.com/iktalk/weather-app-react.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
