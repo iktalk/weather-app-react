822e559a7b456ba32e73b2c18247178ae197cd15
iktalk <iktalk.94@gmail.com>
2022-05-01 14:16:10 -0400

    Update title, add link to Github repository

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <p>
+        This website is coded by Ishmeet Kaur and open source on{" "}
+        <a
+          href="https://github.com/iktalk/weather-app-react.git"
+          target="_blank"
+        >
+          Github
+        </a>
+      </p>
     </div>
   );
 }
